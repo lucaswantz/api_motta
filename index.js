ea276ef16aa7c47dc2537d561ff6c3398662a792
Lucas W. Motta <lucaswantzg@gmail.com>
2024-01-31 07:59:18 -0300

    API AWS: Ajuste na porta

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-	response.json({message: "🚀 Server is running..."});
+	response.json({message: "Health check"});
 });
 
-app.listen(3333);
+app.listen(3333, () => console.log('🚀 Server is running on port 3333...'));
